2f5d057947fd537dee49f75bff6267693c597c34
minidonut <nycom13@gmail.com>
2022-06-11 19:42:53 +0900

    feat: add util packages

--- lambda-namepicker/src/index.ts
+++ lambda-namepicker/src/index.ts
@@ -1,4 +1,7 @@
 import { Handler } from 'aws-lambda';
+import { helloWorld } from '@damgle/utils';
+
+void helloWorld;
 
 const handler: Handler<{ name: string }, { message: string }> = async event => {
   return {
