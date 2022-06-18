5197503d94493564b3abd64ed183f8a3e80ae06c
minidonut <nycom13@gmail.com>
2022-06-18 14:17:18 +0900

    feat(package-errors): impl sample errors for export

--- package-errors/src/index.ts
+++ package-errors/src/index.ts
@@ -1,3 +1,15 @@
-export function helloWorld() {
-  console.log('Hello World!');
-}
+import { createError } from './error.factory';
+import { ErrorType } from './error.type';
+
+const ErrorOf = {
+  User: createError(ErrorType.USER),
+  System: createError(ErrorType.SYSTEM),
+  External: createError(ErrorType.EXTERNAL),
+} as const;
+
+export class NotAuthenticatedError extends ErrorOf.User('Authentication token is invalid', 403) {}
+
+export class MaximumRequestExceedError extends ErrorOf.User(
+  '{{operation}} 요청 제한을 초과했습니다. (max: {{count}})',
+  400
+) {}
