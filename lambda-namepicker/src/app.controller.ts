f2e403d33ede0dceee702d1246b6d5c097a994f2
minidonut <nycom13@gmail.com>
2022-06-19 23:06:59 +0900

    feat(namepicker): add more controller for test

--- lambda-namepicker/src/app.controller.ts
+++ lambda-namepicker/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/:name')
+  getName(@Param('name') name: string): string {
+    return `Hello, ${name}`;
   }
 }
