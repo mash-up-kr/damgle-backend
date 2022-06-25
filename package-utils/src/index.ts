551bbed0664f62663816fcc1869e3665f6864747
minidonut <nycom13@gmail.com>
2022-06-25 13:11:16 +0900

    feat(utils): impl ensuredEnv utils

--- package-utils/src/index.ts
+++ package-utils/src/index.ts
@@ -1,3 +1,24 @@
+import { EnvNotFoundError } from '@damgle/errors';
+
 export function helloWorld() {
   console.log('Hello World!');
+}
+
+export function ensuredEnv<T extends Record<string, string>>(
+  keymap: T,
+  defaults?: Partial<T>
+): { [key in keyof T]: string } {
+  for (const [key, envKey] of Object.entries(keymap)) {
+    const value = process.env[envKey];
+
+    (keymap as any)[key] = value || defaults?.[key];
+
+    if (typeof keymap[key] !== 'string') {
+      throw new EnvNotFoundError({
+        key,
+        envKey,
+      });
+    }
+  }
+  return keymap;
 }
