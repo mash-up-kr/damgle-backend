e06c42078785c2e59bceaa7ba733e794ca2e6933
minidonut <nycom13@gmail.com>
2022-07-09 04:15:47 +0900

    feat(lambda/namepicker): impl name picking logic

--- lambda-namepicker/src/modules/name-picker/name-picker.module.ts
+++ lambda-namepicker/src/modules/name-picker/name-picker.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { NamePickerController } from './name-picker.controller';
 import { NamePickerService } from './name-picker.service';
+import { CandidateCounterSchema, CandidateCounter } from './candidate-counter.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: CandidateCounter.name, schema: CandidateCounterSchema }]),
+  ],
   providers: [NamePickerService],
   controllers: [NamePickerController],
 })
--- lambda-namepicker/src/modules/name-picker/name-picker.service.ts
+++ lambda-namepicker/src/modules/name-picker/name-picker.service.ts
@@ -1,15 +1,88 @@
+import { BadRequestError } from '@damgle/errors';
 import { constant } from '@damgle/utils';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
 import got from 'got';
+import { Model } from 'mongoose';
+import pickRandom from 'pick-random';
+import { Cacheable } from '../../core/cache';
+import { CandidateCounter, CandidateCounterDocument } from './candidate-counter.schema';
 
 @Injectable()
 export class NamePickerService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectModel(CandidateCounter.name)
+    private readonly candidateCounterModel: Model<CandidateCounterDocument>
+  ) {}
 
-  async getHello(): Promise<any> {
-    return got(
+  async getName(): Promise<any> {
+    const { adjectives, nouns } = await this.getCandidateData();
+    const [adjective] = pickRandom(adjectives, { count: 1 });
+    const [noun] = pickRandom(nouns, { count: 1 });
+
+    const key = this.createKey(adjective, noun);
+    const record = await this.candidateCounterModel.findOne({ key });
+    const nth = record == null ? 1 : record.count + 1;
+
+    return {
+      name: this.createFullName(adjective, noun, nth),
+      adjective,
+      noun,
+      nth,
+    };
+  }
+
+  async pickName(adjective: string, noun: string) {
+    const { adjectives, nouns } = await this.getCandidateData();
+    if (!adjectives.includes(adjective)) {
+      throw new BadRequestError({ reason: `${adjective}는 선택할 수 없습니다.` });
+    }
+    if (!nouns.includes(noun)) {
+      throw new BadRequestError({ reason: `${noun}는 선택할 수 없습니다.` });
+    }
+    const nth = await this.atomicIncrement(this.createKey(adjective, noun));
+
+    return {
+      name: this.createFullName(adjective, noun, nth),
+      adjective,
+      noun,
+      nth,
+    };
+  }
+
+  private createKey(adjective: string, noun: string): string {
+    return `${adjective}_${noun}`;
+  }
+
+  private createFullName(adjective: string, noun: string, nth: number): string {
+    return `${adjective} ${this.maybeKorean(nth)}번째 ${noun}`;
+  }
+
+  @Cacheable(180)
+  private async getCandidateData(): Promise<{ adjectives: string[]; nouns: string[] }> {
+    return await got(
       this.configService.getOrThrow('cdnHost') + '/' + constant.s3_namepicker_candidate_path
     ).json();
+  }
+
+  private maybeKorean(nth: number): string {
+    return (
+      ['', '첫', '두', '세', '네', '다섯', '여섯', '일곱', '여덟', '아홉', '열'][nth] ??
+      nth.toString
+    );
+  }
+
+  private async atomicIncrement(key: string) {
+    let result: { count: number } | null = null;
+    while (result == null) {
+      result = await this.candidateCounterModel.findOneAndUpdate(
+        { key },
+        { $inc: { count: 1 } },
+        { upsert: true, returnOriginal: false }
+      );
+    }
+    return result!.count;
   }
 }
