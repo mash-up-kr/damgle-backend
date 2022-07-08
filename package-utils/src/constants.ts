d9c97ede439e2c7f04f1fcae9a5bdc660ef6059a
minidonut <nycom13@gmail.com>
2022-07-09 02:02:31 +0900

    refactor(package/utils): remove redundancy of env loading code

--- package-utils/src/constants.ts
+++ package-utils/src/constants.ts
@@ -1,14 +1,3 @@
-export const S3_NAMEPICKER_CANDIDATE_PATH = 'namepicker/candidates.json';
-
-export const ENV_KEY = {
-  s3_public_bucket: 'S3_PUBLIC_BUCKET',
-  s3_private_bucket: 'S3_PRIVATE_BUCKET',
-  aws_region: 'AWS_REGION',
-  sentry_dsn: 'SENTRY_DSN',
-  namepicker_sheet_id: 'NAMEPICKER_SHEET_ID',
-  gcp_oauth_client_id: 'GCP_OAUTH_CLIENT_ID',
-  gcp_oauth_refresh_token: 'GCP_OAUTH_REFRESH_TOKEN',
-  gcp_oauth_client_secret: 'GCP_OAUTH_CLIENT_SECRET',
-  cdn_host: 'CDN_HOST',
-  api_host: 'API_HOST',
+export const constant = {
+  s3_namepicker_candidate_path: 'namepicker/candidates.json',
 };
--- package-utils/src/index.ts
+++ package-utils/src/index.ts
@@ -1,2 +1,2 @@
-export * from './env';
+export * from './env.static';
 export * from './constants';
