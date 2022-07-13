4191ca31403fa1c8f6750a8a1a02b2929536cc15
minidonut <nycom13@gmail.com>
2022-07-13 18:02:09 +0900

    feat(lambda/auth): add Auth controller

--- lambda-auth/src/modules/auth/auth.controller.ts
+++ lambda-auth/src/modules/auth/auth.controller.ts
@@ -1,14 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Docs } from './auth.docs';
+import { SignInPayload, SignInResult, SignUpPayload, SignUpResult } from './auth.dto';
 import { AuthService } from './auth.service';
+import { AuthorizedRequest } from './auth.type';
+import { JwtAuthGuard } from './jwt/jwt.guard';
 
 @Controller()
 export class AuthController {
   constructor(private readonly auth: AuthService) {}
 
-  @Get()
-  @Docs.hello('hello')
-  async hello() {
-    return await this.auth.hello();
+  @Post('/signup')
+  @Docs.signUp('닉네임으로 유저를 생성합니다.')
+  async signUp(@Body() signUpPayload: SignUpPayload): Promise<SignUpResult> {
+    const [user, accessToken] = await this.auth.signup(signUpPayload);
+    return {
+      accessToken,
+      userNo: user.userNo,
+      nickname: user.nickname,
+      refreshToken: user.refreshToken,
+    };
+  }
+
+  @Post('/signin')
+  @Docs.signIn('refreshToken과 userNo를 사용해서 새로운 액세스 토큰을 발급합니다.')
+  async signIn(@Body() signInPayload: SignInPayload): Promise<SignInResult> {
+    const [user, accessToken] = await this.auth.signin(signInPayload);
+    return {
+      accessToken,
+      userNo: user.userNo,
+      nickname: user.nickname,
+      refreshToken: user.refreshToken,
+    };
+  }
+
+  @Get('/me')
+  @UseGuards(JwtAuthGuard)
+  @Docs.me('내 정보를 가져옵니다.')
+  async me(@Req() req: AuthorizedRequest): Promise<{ userNo: number; nickname: string }> {
+    return {
+      userNo: req.user.userNo,
+      nickname: req.user.nickname,
+    };
   }
 }
