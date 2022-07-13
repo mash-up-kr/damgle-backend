c4a5d8b4ee0a9e13462afc9f61a013500bbf8086
minidonut <nycom13@gmail.com>
2022-07-13 18:02:35 +0900

    feat(lambda/auth): add auth module

--- lambda-auth/src/modules/auth/auth.module.ts
+++ lambda-auth/src/modules/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  User,
+  UserCollectionCounter,
+  UserCollectionCounterSchema,
+  UserSchema,
+} from '../user/user.schema';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserCollectionCounter.name, schema: UserCollectionCounterSchema },
+    ]),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
