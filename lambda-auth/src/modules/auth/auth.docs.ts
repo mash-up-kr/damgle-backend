a500727605427fe738b416198e4a6779161ce883
minidonut <nycom13@gmail.com>
2022-07-13 18:02:13 +0900

    feat(lambda/auth): add Auth docs

--- lambda-auth/src/modules/auth/auth.docs.ts
+++ lambda-auth/src/modules/auth/auth.docs.ts
@@ -1,16 +1,26 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { AuthController } from './auth.controller';
+import { SignInResult, SignUpResult } from './auth.dto';
 
 export type SwaggerMethodDoc<T> = {
   [K in keyof T]: (summary: string) => MethodDecorator;
 };
 
 export const Docs: SwaggerMethodDoc<AuthController> = {
-  hello(summary: string) {
+  signUp(summary: string) {
+    return applyDecorators(ApiOperation({ summary }), ApiOkResponse({ type: SignUpResult }));
+  },
+  signIn(summary: string) {
+    return applyDecorators(ApiOperation({ summary }), ApiOkResponse({ type: SignInResult }));
+  },
+  me(summary: string) {
     return applyDecorators(
-      ApiOperation({ summary })
-      // ApiOkResponse({type: NameResult})
+      ApiBearerAuth(),
+      ApiOperation({ summary }),
+      ApiOkResponse({
+        schema: { properties: { userNo: { type: 'number' }, nickname: { type: 'string' } } },
+      })
     );
   },
 };
