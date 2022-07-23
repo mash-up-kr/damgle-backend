36145e7e478a5a263ec35013cb2f4efbc666a3c2
minidonut <nycom13@gmail.com>
2022-07-23 14:34:05 +0900

    feat(package/utils): impl all-exception-filter

---
+++ package-utils/src/filters/all-exception-filter.ts
@@ -0,0 +1,56 @@
+import { BaseError } from '@damgle/errors';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import Sentry from '@sentry/node';
+import { Response } from 'express';
+import { staticEnv } from '../env.static';
+
+export enum ErrorKind {
+  Http = 'http',
+  Custom = 'custom',
+  Unknown = 'unknown',
+}
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(error: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const { kind, message, status } = this.parseErrorKind(error);
+
+    if (staticEnv.node_env === 'local') {
+      console.error(error);
+    }
+
+    if (kind === ErrorKind.Unknown) {
+      // report unknown error to sentry
+      Sentry.captureException(error);
+    }
+
+    response.status(status).json({ message, status });
+  }
+
+  private parseErrorKind(error: any) {
+    if (error instanceof BaseError) {
+      return {
+        kind: ErrorKind.Custom,
+        status: error.statusCode,
+        message: error.message,
+      };
+    }
+
+    if (typeof error.getStatus === 'function') {
+      // HttpException instanceof 검사가 제대로 안됨. 아마 import하는 객체가 다른듯?
+      return {
+        kind: ErrorKind.Custom,
+        status: error.getStatus(),
+        message: Array.isArray(error.response?.message) ? error.response.message.join('\n') : error.message,
+      };
+    }
+
+    return {
+      kind: ErrorKind.Unknown,
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Unknown server error occurred',
+    };
+  }
+}
